3b82dcd21ddaee7881593e0c010a4143bcf55213
Chris Barnes <barnesc@janelia.hhmi.org>
2019-10-16 17:19:33 -0400

    py38

--- setup.py
+++ setup.py
@@ -27,6 +27,8 @@ setup(
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.4',
